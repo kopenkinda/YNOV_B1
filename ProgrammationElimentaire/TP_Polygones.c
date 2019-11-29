ad465c9fa41ed7dd902a7a0c7ffdb81e08ee4d9a
kopenkinda <kopenkin.da@gmail.com>
2019-11-29 14:45:10 +0100

    Added Polygon Ver 3

--- ProgrammationElimentaire/TP_Polygones.c
+++ ProgrammationElimentaire/TP_Polygones.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <math.h>
 
-struct polygone_6
+struct polygon_6
 {
   int coord1[2];
   int coord2[2];
@@ -12,7 +12,7 @@ struct polygone_6
 };
 
 // ? Ver 1
-// float calcP6(struct polygone_6 pol)
+// float calcP6(struct polygon_6 pol)
 // {
 //   float result = 0;
 //   result += sqrt(pow(pol.coord2[1] - pol.coord1[1], 2) + pow(pol.coord2[0] - pol.coord1[0], 2));
@@ -24,36 +24,65 @@ struct polygone_6
 //   return result;
 // };
 
-
 // ? Ver 2
-double calcP6(struct polygone_6 pol)
+// double calcP6(struct polygon_6 pol)
+// {
+//   double result = 0;
+//   int
+//       x[6] = {pol.coord1[0], pol.coord2[0], pol.coord3[0], pol.coord4[0], pol.coord5[0], pol.coord6[0]},
+//       y[6] = {pol.coord1[1], pol.coord2[1], pol.coord3[1], pol.coord4[1], pol.coord5[1], pol.coord6[1]},
+//       j;
+//   for (int i = 0; i < 6; i++)
+//   {
+//     j = i + 1;
+//     if (j == 6)
+//       j = 0;
+//     result += sqrt(pow(x[j] - x[i], 2) + pow(y[j] - y[i], 2));
+//   }
+//   return result;
+// };
+
+// ? Ver 3
+typedef struct Point Point;
+struct Point
+{
+  double x;
+  double y;
+};
+
+double calc_polygon(Point *p, int l)
 {
   double result = 0;
-  int
-      x[6] = {pol.coord1[0], pol.coord2[0], pol.coord3[0], pol.coord4[0], pol.coord5[0], pol.coord6[0]},
-      y[6] = {pol.coord1[1], pol.coord2[1], pol.coord3[1], pol.coord4[1], pol.coord5[1], pol.coord6[1]},
-      j;
-  for (int i = 0; i < 6; i++)
+  int j;
+  for (int i = 0; i < l; i++)
   {
-    j = i + 1;
-    if (j == 6)
-      j = 0;
-    result += sqrt(pow(x[j] - x[i], 2) + pow(y[j] - y[i], 2));
+    j = (i == l - 1) ? 0 : i + 1;
+    result += sqrt(pow(p[j].x - p[i].x, 2) + pow(p[j].y - p[i].y, 2));
   }
   return result;
-};
+}
 
 int main()
 {
-  struct polygone_6 myPolygone = {
+  // ! Ver 1 || Ver 2
+  // struct polygon_6 mypolygon = {
+  //     {2, 0},
+  //     {6, 0},
+  //     {10, -2},
+  //     {8, -6},
+  //     {2, -8},
+  //     {0, -4}
+  //     };
+  // double result = calcP6(mypolygon);
+  // ! Ver 3
+  Point polygon[] = {
       {2, 0},
       {6, 0},
       {10, -2},
       {8, -6},
       {2, -8},
       {0, -4}};
-
-  double result = calcP6(myPolygone);
+  double result = calc_polygon(polygon, sizeof(polygon) / sizeof(polygon[0]));
   printf("The Perimeter is -> %.2f\n", result);
   return 0;
 }
