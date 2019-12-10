498d8223cfca80fc8362ca7575e5772112e3641b
kopenkinda <kopenkin.da@gmail.com>
2019-12-10 14:14:25 +0100

    Updated bowling.js - Made readable commentary

--- JavaScript/bowling.js
+++ JavaScript/bowling.js
@@ -1,3 +1,13 @@
+//? TP
+//? 10 tirs au lancer au total
+//? chaque lancé a 2 boules
+//? en cas de spare (10 quilles tombé en 2 boules), on ajoute
+//? le score de la prochaine boule à ce score
+//? en cas de strike (10 quilles en 1 boule),
+//? on ajoute le score des 2 prochaines boules
+//? le score maximum est de 300
+//? sur le dernier tir en cas de strike on ajoutera 2 boules au tir
+
 const bowling = {
   p1: { throws: [], score: 0 },
   // p2: { throws: [], score: 0 },
@@ -26,7 +36,7 @@ const countScore = playerAll =>
         let throw1 = player.throws[ i ][ 0 ];
         let throw2 = player.throws[ i ][ 1 ];
 
-        // ? Strike
+        // * Strike
         if ( throw1 == 10 )
         {
           console.log( `Player${ index + 1 }: ${ throw1 } - ${ throw2 } Strike` );
@@ -39,7 +49,7 @@ const countScore = playerAll =>
             player.score += throw1 + throw2 + throw3 + Math.round( Math.random() * ( 10 - throw3 ) );
           }
         }
-        // ? Spare
+        // * Spare
         else if ( ( throw1 + throw2 ) == 10 || throw2 == 10 )
         {
           console.log( `Player${ index + 1 }: ${ throw1 } - ${ throw2 } Spare` );
@@ -52,7 +62,7 @@ const countScore = playerAll =>
             player.score += throw1 + throw2 + throw3;
           }
         }
-        // ? Normal throw
+        // * Normal throw
         else
         {
           console.log( `Player${ index + 1 }: ${ throw1 } - ${ throw2 }` )
@@ -64,7 +74,7 @@ const countScore = playerAll =>
   } )
 };
 
-generateThrows( [ bowling.p1], 10 );
+generateThrows( [ bowling.p1 ], 10 );
 countScore( [ bowling.p1 ] );
 console.log( bowling.p1.score );
 // generateThrows( [ bowling.p1, bowling.p2], 10 );
